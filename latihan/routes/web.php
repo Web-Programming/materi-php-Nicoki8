aebafe90004f47f5ebc743d438ea17d724892877
awa <Zanesen_2327240066@mhs.mdp.ac.id>
2025-04-29 20:35:05 +0700

    Latihan

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function(){
+    return view('beranda',
+    [
+        'name' =>'Nico',
+        'email' => 'nico@gmail.com',
+        'alamat' => 'Palembang',
+    ]);
+});
+
+Route::get('/berita/{id}/{judul?}', function ($id,$judul = null) {
+    return view('berita', ['id' => $id,'judul' => $judul]);
+});
